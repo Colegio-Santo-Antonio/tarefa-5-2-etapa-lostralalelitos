e8537474cf0e826b6b424d8e8035da9de35b3d6c
skibidi-sigma-lover <csa20-9816@colegiosantoantonio.com.br>
2025-09-02 16:33:09 -0300

    Update student.py

--- student.py
+++ student.py
@@ -1,16 +1,17 @@
 # Leia uma linha com o número do cartão
 numero = input()
-lista = [int(digito) for digito in str(numero)]
+lista = [int(digito) for digito in numero]
 invertida = lista(::-1)
 
 
 npi = invertida[1::2]
 
 npp = invertida[::2]
-l3 = npi + npp + npp
+
+a = sum(npi) + sum(npp)
 
 divisor = 10
-if l3 % divisor == 0:
+if a % divisor == 0:
     print("Cartão válido")
 else:
     print("Cartão inválido")
