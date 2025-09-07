2516e93affece1b1027f72d76a6fc0dcd2b6129b
skibidi-sigma-lover <csa20-9816@colegiosantoantonio.com.br>
2025-09-07 15:11:29 -0300

    t15.py

--- student.py
+++ student.py
@@ -1,23 +1,17 @@
 numero = input()
-lista = [int(digito) for digito in numero]
-invertida = lista[::-1]
+im = [int(i) for i in numero[-1::2]] 
+pa = []
+for i in numero[=2::2]:
+    z = int(i)
+    if 2*z < 10:
+        pa.append(2*z)
+    else:
+        pa.append(2*z - 9)
 
-npi = invertida[1::2]  
-npp = invertida[::2]   
-
-npp_corrigido = []
-for a in npp:
-    dobro = a * 2
-    if dobro > 9:
-        dobro -= 9
-    npp_corrigido.append(dobro)
-
-p = sum(npi) + sum(npp_corrigido)
-
-if p % 10 == 0:
+soma = sum(pa) + sum(im)
+if soma % 10 ==0:
     print("Cartão válido")
 else:
     print("Cartão inválido")
-
 
 # Feito por Mateus Loures, Davi Versiani e João Vitor Toscano do 1ºF
